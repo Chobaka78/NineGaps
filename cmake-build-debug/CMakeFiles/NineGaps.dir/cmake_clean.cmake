0e872ec9b3b303780db6e67b05f44c893361dae7
Chobaka78 <34846941+Chobaka78@users.noreply.github.com>
2019-09-24 01:08:05 -0400

    Initial Comment

---
+++ cmake-build-debug/CMakeFiles/NineGaps.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NineGaps.dir/main.c.obj"
+  "NineGaps.pdb"
+  "NineGaps.exe"
+  "NineGaps.exe.manifest"
+  "libNineGaps.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NineGaps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
